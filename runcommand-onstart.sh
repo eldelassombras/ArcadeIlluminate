93d34fd5b632b1f099f25608e52d571d621cbe40
Claudio <4069247+eldelassombras@users.noreply.github.com>
2020-03-16 11:55:22 -0300

    Update runcommand-onstart.sh

--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -4,5 +4,5 @@ sistema="$1"
 ruta_rom="$4"
 extension_rom="${ruta_rom##*/}"
 nombre_rom="${extension_rom%.*}"
-/home/pi/illuminate/illuminate.py "$sistema" "$nombre_rom" &>/tmp/illuminate.log
+/home/pi/ArcadeIlluminate/illuminate.py "$sistema" "$nombre_rom" &>/tmp/illuminate.log
 # End-Illumination #
